c49410e6d611aca7636cbd459fa595ff29d74006
Curtis Larsen <curtis.gale.larsen@gmail.com>
2025-03-28 16:42:08 -0600

    rename source to winona package

---
+++ main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import winona.cli.main
+
+winona.cli.main.main()
+
---
+++ winona/cli/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from ..database import DatabaseManager
+
+def main():
+    print("CLI")
