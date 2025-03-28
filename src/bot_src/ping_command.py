8a053d28cd2fd9aa151eab35e9c654d55baabad0
Curtis Larsen <curtis.gale.larsen@gmail.com>
2025-03-28 15:08:30 -0600

    /winona utility ping

--- src/bot_src/ping_command.py
+++ src/bot_src/ping_command.py
@@ -2,16 +2,20 @@
 
 import interactions
 
-class PingExtension(interactions.Extension):
+class PingCommand(interactions.Extension):
     def __init__(self, client):
         self.client: interactions.Client = client
 
     @interactions.slash_command(
-        name="ping",
-        description="Replies with pong!",
+        name="winona",
+        description="Winona Bot commands.",
+        group_name="utility",
+        group_description="Utility commands.",
+        sub_cmd_name="ping",
+        sub_cmd_description="Responds with the bot's latency.",
     )
-    async def ping(self, ctx: interactions.SlashContext):
-        await ctx.send("Pong!")
+    async def ping_command(self, ctx: interactions.SlashContext):
+        await ctx.send(f"Pong! Latency: {round(self.client.latency, 1)}ms")
 
 def setup(client):
-    PingExtension(client)
+    PingCommands(client)
